7fcdaa75a4ccb7de60dbdd2783813019028b5716
tomerikheggedal <92306093+tomerikheggedal@users.noreply.github.com>
2022-09-16 20:41:55 +0200

    Initial Commit

---
+++ War-challenge/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  War-challenge
+//
+//  Created by Tom erik Heggedal on 16/09/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ War-challenge/War_challengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  War_challengeApp.swift
+//  War-challenge
+//
+//  Created by Tom erik Heggedal on 16/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct War_challengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
